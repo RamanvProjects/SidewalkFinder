5a8b69fa6739d133e7e7bbbb5f7dd45117c1ba5e
Vivek Ramanujan <ramanv@cs.washington.edu>
2015-04-26 00:06:35 -0700

    Processing sketch made

---
+++ SidewalkFinder.pde
@@ -0,0 +1,9 @@
+//First experimentation with using processing as a visualization system
+
+void setup() {	
+  size(800, 400, "processing.core.PGraphicsRetina2D");
+}
+
+void draw() {
+	
+}
---
+++ build-tmp/source/SidewalkFinder.java
@@ -0,0 +1,34 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SidewalkFinder extends PApplet {
+
+//First experimentation with using processing as a visualization system
+
+public void setup() {	
+  size(800, 400, "processing.core.PGraphicsRetina2D");
+}
+
+public void draw() {
+	
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SidewalkFinder" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
